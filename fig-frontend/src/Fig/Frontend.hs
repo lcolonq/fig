15e38cc33c427bf980c4fdfb3bc80da98955aafe
LLLL Colonq <llll@colonq>
2024-01-12 16:28:15 -0500

    Remove junk

--- fig-frontend/src/Fig/Frontend.hs
+++ fig-frontend/src/Fig/Frontend.hs
@@ -37,20 +37,10 @@ app cfg = do
       . L.renderBS
       $ L.doctypehtml_ do
           L.head_ do
-            L.title_ "The Junkyard"
-            L.link_ [L.rel_ "stylesheet", L.href_ "js/index.css"]
-            L.link_ [L.rel_ "stylesheet", L.href_ "https://fonts.googleapis.com/css?family=Rubik+Maps"]
+            L.title_ "clonk zone api home page"
             L.link_ [L.rel_ "icon", L.href_ "data:;base64,iVBORw0KGgo="]
-            L.script_ [L.type_ "module", L.src_ "js/index.js"] ("" :: L.Html ())
           L.body_ do
-            L.term "fig-backdrop" ""
-            L.term "fig-header" ""
-            L.term "fig-footer" ""
-            window "test1" "window one" do
-              L.p_ "Deserunt consequatur neque autem. Dicta assumenda autem consequatur sunt animi. Dolor voluptatem rerum aut id ut vel. Labore soluta itaque voluptas dolores repellat. Voluptatem dolor fugit necessitatibus amet et natus velit. Enim nihil voluptate qui quasi architecto. Reprehenderit porro aperiam eaque sequi veritatis in quos. Odio tenetur labore cupiditate nisi. Deserunt aut dolore consequuntur inventore quod veniam commodi. Incidunt qui sequi dolor. Quia adipisci dolores ab. Dolor molestias est earum ea. Possimus ullam repellat qui consequatur dolorem excepturi non. Incidunt magnam quaerat temporibus quisquam. Vero nihil possimus ratione voluptas sunt. Qui quisquam ipsa omnis est totam iure odio. Ab fugiat in id nisi. Dolor sit est soluta eaque ut eveniet. Eveniet rerum fuga doloremque repellendus eligendi sunt asperiores. Tenetur iure vitae ea sapiente et. Aspernatur natus aut rerum magnam occaecati. Veritatis exercitationem necessitatibus assumenda est assumenda eaque molestiae."
-            window "test2" "window two" do
-              L.h1_ "hello"
-            L.term "fig-gizmo" [L.id_ "gizmo1"] ""
+            "hello"
     , Tw.get "/api/check" $ authed cfg \auth -> do
         Tw.send $ Tw.json @[Text] [auth.id, auth.name]
     , Tw.put "/api/buffer" do
