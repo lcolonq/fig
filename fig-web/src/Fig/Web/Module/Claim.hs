f40026979afbedabc4d2c8ad695f616b99a3dd76
LLLL Colonq <llll@colonq>
2026-07-21 03:19:26 -0400

    web: Actually, don't make them consumable

--- fig-web/src/Fig/Web/Module/Claim.hs
+++ fig-web/src/Fig/Web/Module/Claim.hs
@@ -11,13 +11,16 @@ import qualified Fig.Utils.DB as DB
 
 secure :: SecureModule
 secure a = do
-  onPost "/api/claim/:code" $ authed a \creds -> do
+  onGet "/api/claim/:code" $ authed a \creds -> do
     code <- pathParam "code"
     DB.run a.db (DB.hget "claim:codes" code) >>= \case
-      Nothing -> log $ "user " <> creds.user <> " tried invalid badge claim code: " <> decodeUtf8 code
+      Nothing -> do
+        log $ "user " <> creds.user <> " tried invalid badge claim code: " <> decodeUtf8 code
+        status status400
+        respondText "invalid claim code"
       Just badge -> do
         log $ "user " <> creds.user <> " redeemed valid badge claim code: " <> decodeUtf8 code
         let bstid = encodeUtf8 creds.twitchId
         DB.run a.db do
-          DB.hdel "claim:codes" code
           DB.sadd ("user:badges:" <> bstid) [badge]
+        redirect $ "https://api.colonq.computer/charsheet#" <> creds.twitchId
