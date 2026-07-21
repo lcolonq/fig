657372bef7cede43aae8cd13eda6426487e3c965
LLLL Colonq <llll@colonq>
2026-07-21 03:16:04 -0400

    web: Make badge claim codes single-use

--- fig-web/src/Fig/Web/Module/Claim.hs
+++ fig-web/src/Fig/Web/Module/Claim.hs
@@ -13,10 +13,11 @@ secure :: SecureModule
 secure a = do
   onPost "/api/claim/:code" $ authed a \creds -> do
     code <- pathParam "code"
-    DB.run a.db (DB.hget code "claim:codes") >>= \case
+    DB.run a.db (DB.hget "claim:codes" code) >>= \case
       Nothing -> log $ "user " <> creds.user <> " tried invalid badge claim code: " <> decodeUtf8 code
       Just badge -> do
         log $ "user " <> creds.user <> " redeemed valid badge claim code: " <> decodeUtf8 code
         let bstid = encodeUtf8 creds.twitchId
         DB.run a.db do
+          DB.hdel "claim:codes" code
           DB.sadd ("user:badges:" <> bstid) [badge]
